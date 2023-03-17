ccfd51fc6769e46d4f685ae477faee576dd76c6e
GetSerious495 <61726236+GetSerious495@users.noreply.github.com>
2023-03-17 17:51:47 +0700

    Add files via upload

---
+++ MyProject/Diff_Example.py
@@ -0,0 +1,17 @@
+import sympy as sy
+
+t = sy.symbols('t')
+
+Q1 = t**5 - 12*t**2 - 9
+
+sy.plot(Q1)
+
+dq1 = sy.diff(Q1,t)
+
+sy.plot(dq1)
+print(dq1)
+
+dq2 = sy.diff(dq1,t)
+
+sy.plot(dq2)
+print(dq2)
