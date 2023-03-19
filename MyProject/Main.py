f18d0ee9b88696b7664076d2dfd91f2229ffebcd
GetSerious495 <61726236+GetSerious495@users.noreply.github.com>
2023-03-19 22:11:06 +0700

    Add files via upload

---
+++ MyProject/Main.py
@@ -0,0 +1,12 @@
+import Derivative
+import Integral
+import Diff_Example
+import Int_Example
+import sympy
+
+x,y = sympy.symbols("x y")
+
+Derivative.accel_diff(x**2)
+Integral.accel_int(2*x)
+Diff_Example.diff_ex()
+Int_Example.int_ex()
